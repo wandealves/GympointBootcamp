cb7dd375e34952af2a7f424e1ea5b41c4979ebde
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-12-06 16:12:34 -0300

    Foi criado os estilos especificos e o global estilo

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Route from './Route';
 
@@ -18,11 +18,11 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashborad" component={Dashborad} isPrivate />
-      <Route path="/helpOrders" component={HelpOrder}  isPrivate/>
-      <Route path="/plans" component={Plan} isPrivate/>
-      <Route path="/registrations" component={Registration} isPrivate/>
-      <Route path="/students" component={Student} isPrivate/>
+      <Route path="/dashboard" component={Dashborad} isPrivate />
+      <Route path="/helpOrders" component={HelpOrder} isPrivate />
+      <Route path="/plans" component={Plan} isPrivate />
+      <Route path="/registrations" component={Registration} isPrivate />
+      <Route path="/students" component={Student} isPrivate />
     </Switch>
   );
 }
