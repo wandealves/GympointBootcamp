af064ebf4913274b3f8e35025700026e27f90ce4
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-12-07 10:23:17 -0300

    Ajuste form para pegar os dados do login

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <div>
         <img src={logo} alt="Gympoint" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <span htmlFor="email">SEU E-MAIL</span>
-          <input id="email" type="email" placeholder="example@email.com" />
+          <Input
+            name="email"
+            id="email"
+            type="email"
+            placeholder="example@email.com"
+          />
           <span htmlFor="password">SUA SENHA</span>
-          <input id="password" type="password" placeholder="**************" />
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="**************"
+          />
 
           <button type="submit">Entrar no sistema</button>
           <Link to="/register">Criar conta</Link>
-        </form>
+        </Form>
       </div>
     </>
   );
--- web/src/pages/SingUp/index.js
+++ web/src/pages/SingUp/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Form, Input } from '@rocketseat/unform';
 import logo from '~/assets/logo.png';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <div className="container">
         <img src={logo} alt="Gympoint" />
 
-        <form>
-          <input placeholder="Nome Completo" />
-          <input type="email" placeholder="Seu e-mail" />
-          <input type="password" placeholder="Sua senha" />
+        <Form onSubmit={handleSubmit}>
+          <Input name="name" placeholder="Nome Completo" />
+          <Input name="email" type="email" placeholder="Seu e-mail" />
+          <Input name="password" type="password" placeholder="Sua senha" />
 
           <button type="submit">Criar Conta</button>
           <Link to="/">Já tenho login</Link>
-        </form>
+        </Form>
       </div>
     </>
   );
