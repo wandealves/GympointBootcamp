8098e8f043dbda05da1063b3d58603f9115b481a
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-12-15 10:05:42 -0300

    add actions, sagas e reducer auth

--- mobile/GympointApp/src/config/ReactotronConfig.js
+++ mobile/GympointApp/src/config/ReactotronConfig.js
@@ -1,9 +1,12 @@
 import Reactotron from 'reactotron-react-native';
 import {reactotronRedux} from 'reactotron-redux';
 import reactotronsaga from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
+
 
 if(__DEV__){
   const tron = Reactotron
+  .setAsyncStorageHandler(AsyncStorage)
   .configure({host: '192.168.1.106'})
   .useReactNative()
   .use(reactotronRedux())
--- mobile/GympointApp/src/index.js
+++ mobile/GympointApp/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
 import {StatusBar} from 'react-native';
 
 import './config/ReactotronConfig';
+import {store, persistor} from './store';
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <StatusBar barStyle="light-content" backgroundColor="#ee4d64"/>
       <Routes/>
-    </>
+      </PersistGate>
+    </Provider>
   );
 }
